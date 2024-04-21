1a65ab8b4903d88002fd9beaf2caa29ff9bc98ca
kyrylfes <kirill.fesiuk@knu.ua>
2024-04-21 21:59:14 +0200

    Create base.html

--- web/views.py
+++ web/views.py
@@ -6,3 +6,11 @@ from web.models import UserProfile
 def profile(request):
     profiles = UserProfile.objects.all()
     return render(request, 'profile.html', {'profiles': profiles})
+
+
+def materials(request):
+    return render(request, 'library.html')
+
+
+def books(request):
+    return render(request, 'profile.html')
